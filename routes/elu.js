11dae73a5b6442c266fb6b9d50e5fbb53b3b430a
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-08-25 10:37:07 +0200

    Update elu.js

--- routes/elu.js
+++ routes/elu.js
@@ -7,6 +7,6 @@ module.exports = async function (fastify, opts) {
   fastify.get('/metrics', async (request, reply) => {
     request.log.info("GET /metrics")
     const metricsData = await prometheus.register.metrics()
-    reply.send(metricsData)
+    return metricsData
   })
 }
